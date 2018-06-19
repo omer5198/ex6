1773cba36098998610d3a954a88e1c9120a4a2c9
RoniVarshavsky <roni.varshavsky@mail.huji.ac.il>
2018-06-19 20:14:45 +0300

    started project

---
+++ src/oop/ex6/main/Variable.java
@@ -0,0 +1,18 @@
+package oop.ex6.main;
+
+public class Variable {
+
+    private String type;
+
+    private String name;
+
+    private boolean initialized;
+
+    public Variable(String type, String name, boolean isInitialized) {
+        this.type = type;
+        this.name = name;
+        this.initialized = initialized;
+    }
+
+
+}
